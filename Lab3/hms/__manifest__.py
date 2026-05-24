585256f8b400771924c6ba2b2250aa0234e959af
Sohyla Gomaa <sohylaelqourashi@gmail.com>
2026-05-25 01:59:53 +0300

    add Lab3

---
+++ Lab3/hms/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'HMS - Hospital Management System',
+    'version': '1.0',
+    'author': 'sohyla',
+    'license': 'LGPL-3',
+    'category': 'Healthcare',
+    'depends': ['base', 'contacts', 'sale_management'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hms_department_views.xml',
+        'views/hms_doctors_views.xml',
+        'views/hms_patient_views.xml',
+        'views/inherited_res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
